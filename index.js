0b967adeba0b075226c4a6f28f13881c623e6f68
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-03-23 03:42:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function produceDrivingRange(blockRange) {
          }
     }
   }
-  
+
   function produceTipCalculator( percentage ) {
     return function ( rideFare ) {
       return rideFare * percentage;
-    }  
+    }
   }
 
   function createDriver() {
